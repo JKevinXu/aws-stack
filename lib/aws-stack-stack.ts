8eab480aa6285b28aaadcd5f561b17270bd3ef16
Kevin Xu <xkevinj@gmail.com>
2025-04-29 16:24:54 +0800

    Add IAM Identity Center instance to AWS CDK stack

--- lib/aws-stack-stack.ts
+++ lib/aws-stack-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sso from 'aws-cdk-lib/aws-sso';
 
 export class AwsStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +21,21 @@ export class AwsStackStack extends cdk.Stack {
       encryption: s3.BucketEncryption.S3_MANAGED,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production
       autoDeleteObjects: true, // NOT recommended for production
+    });
+
+    // Create an IAM Identity Center instance
+    const identityCenterInstance = new sso.CfnInstance(this, 'MyIdentityCenterInstance', {
+      name: 'My-Identity-Center',
+      tags: [
+        {
+          key: 'Environment',
+          value: 'Development'
+        },
+        {
+          key: 'Purpose',
+          value: 'Identity-Management'
+        }
+      ]
     });
   }
 }
